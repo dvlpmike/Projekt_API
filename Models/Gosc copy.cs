b1c52421f727f58d57567dd422755ecc844de131
mgliwicki <mgliwicki@gmail.com>
2020-11-09 12:39:32 +0100

    new branch

---
+++ Models/Gosc copy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt_API.Models
+{
+    public class Gosc
+    {
+        public Gosc()
+        {
+            Rezerwacja = new HashSet<Rezerwacja>();
+        }
+
+        public int IdGosc { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public int? ProcentRabatu { get; set; }
+
+        public virtual ICollection<Rezerwacja> Rezerwacja { get; set; }
+    }
+}
